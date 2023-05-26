6dab59509cba8a2c940717f9356a93e6c6c65e4c
Felipe <flpc.dev@gmail.com>
2023-05-26 16:30:47 -0300

    feat: dotenv treatment with zod

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
+import { env } from './env'
 
 const app = fastify()
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log(`server listening on 3333`)
+    console.log(`server listening on ${env.PORT}`)
   })
