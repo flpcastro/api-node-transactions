fcfc94c13a690440ffd88830d3fcf4e469d72203
Felipe <flpc.dev@gmail.com>
2023-05-27 04:41:29 -0300

    test: create first e2e test

---
+++ test/transacitons.spec.ts
@@ -0,0 +1,25 @@
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+
+import { app } from '../src/app'
+
+describe('Transactions Routes', () => {
+  beforeAll(() => {
+    app.ready()
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+
+  it('should be able to user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
