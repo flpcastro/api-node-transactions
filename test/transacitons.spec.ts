e10eb3be4fa1cae448d65e3bfe2f4b21080acc67
Felipe <flpc.dev@gmail.com>
2023-05-27 05:22:50 -0300

    test: create get transaction by id e2e test

--- test/transacitons.spec.ts
+++ test/transacitons.spec.ts
@@ -1,6 +1,7 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import request from 'supertest'
 
+import { execSync } from 'child_process'
 import { app } from '../src/app'
 
 describe('Transactions Routes', () => {
@@ -10,6 +11,11 @@ describe('Transactions Routes', () => {
 
   afterAll(() => {
     app.close()
+  })
+
+  beforeEach(() => {
+    execSync('yarn knex migrate:rollback --all')
+    execSync('yarn knex migrate:latest')
   })
 
   it('should be able to user can create a new transaction', async () => {
@@ -45,5 +51,36 @@ describe('Transactions Routes', () => {
         amount: 5000,
       }),
     ])
+  })
+
+  it('should be able to get a specific transaction by id', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionByIdResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionByIdResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
   })
 })
